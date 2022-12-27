a247bcadec5fae5f155afd4c5a8d919d538e00e7
overlordpro-sys <64398319+overlordpro-sys@users.noreply.github.com>
2022-12-27 13:58:59 -0800

    Revert "Implemented Cards"
    This reverts commit 13704c117662d38f652e97039547bb53ffa25a23.

--- js/courses.js
+++ js/courses.js
@@ -1,25 +1,16 @@
-$('button.filter').on('click', function(){
+window.addEventListener("slide.bs.carousel", function(e) {
+    let child = e.relatedTarget
+    let index = [].indexOf.call(child.parentNode.children, child);
 
-    const cards = Array.from(document.querySelectorAll('.card'));
-    const buttons = Array.from(document.querySelectorAll('.filter'));
-    for(card of cards){
-        card.parentElement.style.display = 'none';
+    let forSlide = document.querySelector(".for-slide-" + index);
+
+    console.log("should show collapse " + forSlide.innerText.trim())
+    return;
+
+    // check your collapse code currently doesn't work
+
+    if (!forSlide.classList.contains("in")) {
+        document.querySelector("#collapseGroup>.collapse.show").collapse("hide");
+        forSlide.collapse("show");
     }
-
-    for(button of buttons){
-        button.classList.remove('active');
-        button.removeAttribute('aria-pressed');
-    }
-    this.classList.add('active');
-    this.setAttribute('aria-pressed', 'true');
-
-    const categoryOne = this.getAttribute('category-one');
-    const categoryTwo = this.getAttribute('category-two');
-
-    const result = cards.filter(card => card.getAttribute('category') === categoryOne || card.getAttribute('category') === categoryTwo || categoryOne === 'all');
-
-    for(card of result){
-        card.parentElement.style.display = 'block';
-    }
-
 });
